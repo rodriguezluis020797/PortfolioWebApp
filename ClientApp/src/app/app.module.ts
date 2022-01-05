5586871e82c87254738b239a6842f91dee20b368
rodriguezluis2797 <76459394+rodriguezluis2797@users.noreply.github.com>
2022-01-05 01:22:47 -0800

    Commit

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -20,6 +22,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'resume', component: ResumeComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
---
+++ ClientApp/src/app/resume/resume.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'resume',
+  templateUrl: './resume.component.html',
+})
+export class ResumeComponent implements OnInit{
+    
+    ngOnInit(): void {
+      
+    }
+}
